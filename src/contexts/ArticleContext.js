d41d56ccabce9b799b69670300ec15d6c3e845ac
Louis Moselhi <louis.moselhi@bbc.co.uk>
2019-07-19 12:19:36 +0100

    move data to ArticleContext

--- src/contexts/ArticleContext.js
+++ src/contexts/ArticleContext.js
@@ -1,4 +1,9 @@
 import React, { useState, createContext, useEffect } from 'react';
+import article1data from "../articleData/article1";
+import article2data from "../articleData/article2";
+import article3data from "../articleData/article3";
+import article4data from "../articleData/article4";
+import article5data from "../articleData/article5";
 
 export const ArticleContext = createContext()
 
@@ -17,31 +22,11 @@ export const ArticleProvider = (props) => {
   // }
 
   const [articles, setArticles] = useState([
-    {
-      title: 'Article 1',
-      body: 'Lorem ipsum of the first article',
-      id: 0
-    },
-    {
-      title: 'Article 2',
-      body: 'Lorem ipsum of the second article',
-      id: 1
-    },
-    {
-      title: 'Article 3',
-      body: 'Lorem ipsum  of the third article',
-      id: 2
-    },
-    {
-      title: 'Article 4',
-      body: 'Lorem ipsum  of the fourth article',
-      id: 3
-    },
-    {
-      title: 'Article 5',
-      body: 'Lorem ipsum  of the fifth article',
-      id: 4
-    }
+    article1data,
+    article2data,
+    article3data,
+    article4data,
+    article5data
   ])
 
   return(
